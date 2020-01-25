efc48d57b6ee45acd279510ca165367577ecdd63
Pako <kirill.mitin34@gmail.com>
2020-01-25 13:49:44 +0300

    3 lesson done

--- js/main.js
+++ js/main.js
@@ -1,35 +1,121 @@
-const renderGoodsItem = (title, price, img = '') => {
-    return `<div class="goods-item">
-        <img src="${img}" alt="image">
-        <div class="desc">
-            <h3>${title}</h3>
-            <p>${price}</p>
-            <button>Добавить</button>
-        </div>
-    </div>`
-};
-const goods = [
-    {title: "Машина", price: 150, img: 'https://via.placeholder.com/250'}, 
-    {title: "Машина-красная", price: 175, img: 'https://via.placeholder.com/250'},
-    {title: "Машина-синия", price: 180, img: 'https://via.placeholder.com/250'},
-    {title: "Машина-зелёная", price: 185, img: 'https://via.placeholder.com/250'},
-    {title: "Машина-чёрная", price: 190, img: 'https://via.placeholder.com/250'},
-];
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-const renderGoodsList = (list, container) => {
-    const goodsList = list.map(good => renderGoodsItem(good.title, good.price, good.img))
-    document.querySelector(container).innerHTML = goodsList.join('');
-};
-//1. Добавьте пустые классы для корзины товаров и элемента корзины товаров. Продумайте, какие методы понадобятся для работы с этими сущностями.
-class GoodsList {
-    constructor() {
-        this.goods = [];
+function makeGetRequest(url, callback) {
+    let xhr;
+    if (window.XMLHttpRequest) {
+        xhr = new window.XMLHttpRequest()
+    } else {
+        xhr = new window.ActiveXObject("Microsoft.XMLHTTP")
     }
-//2. Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.
-total() {
-    let totalPrice = 0;
-    this.goods.forEach(item=> totalPrice + item.price);
-    return totalPrice;
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            callback(xhr.responseText) //обработка
+        }
+    };
+
+    xhr.open('GET', url);
+    xhr.send();
+}
+
+class GoodsItem {
+    constructor(id, title = 'Без навания', price = 0, img = 'https://via.placeholder.com/250') {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="goods-item">
+            <img src="${this.img}" alt="image">
+            <div class="desc">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="js-add-to-cart">Добавить</button>
+            </div>
+        </div>
+        `;
     }
 }
-renderGoodsList(goods, '.goods-list');
+class GoodsList {
+    constructor(container) {
+        this.container = document.querySelector(container);
+        this.goods = [];
+    }
+    initListeners() {}
+    findGood(id) {
+        return this.goods.find(good => good.id === id);
+    }
+
+    fetchGoods() {}
+    totalSum() {
+        let sum = 0;
+        for (const good of this.goods) {
+            if(good.price) {
+                sum += good.price;
+            }
+        }
+        return sum;
+    }
+
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.id, good.product_name, good.price, good.img);
+            listHtml += goodItem.render();
+        })
+        this.container.innerHTML = listHtml;
+        this.initListeners();
+    }
+}
+class GoodsPage extends GoodsList{
+    initListeners() {
+        const buttons = [... this.container.querySelectorAll('.js-add-to-cart')];
+        buttons.forEach(button => {
+            button.addEventListener('click', (event) => {
+                const goodId = event.target.parentElement.getAttribute('data-id');
+                this.addToCart(parseInt(goodId, 10));
+            })
+        })
+    }
+    fetchGoods(callback) {
+        makeGetRequest(`${API_URL}/catalogData.json`, (goods) => {
+            this.goods = JSON.parse(goods);
+        callback();
+        })
+    }
+    addToCart(goodId) {
+        const good = this.findGood(goodId);
+        console.log(good);
+    }
+}
+class Cart extends GoodsList {
+    removeFromCart(goodId) {
+
+    }
+    cleanCart() {
+
+    }
+    updateCartItem(id, goods) {
+
+    }
+}
+
+class CartItem extends GoodsItem{
+    constructor(...attrs) {
+        super(attrs);
+        this.count = 0;
+    }
+    incCount() {
+        
+    }
+    decCount() {
+
+    }
+}
+
+const list = new GoodsPage('.goods-list');
+list.fetchGoods(() => {
+    list.render();
+});
+console.log(list.totalSum());
